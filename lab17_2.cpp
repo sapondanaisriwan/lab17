b566ab7cf7c5f36a705083d3c5a07f0024181a4d
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-02-05 22:30:16 +0700

    .

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&p, int N)
+{
+	p = new char[N];
+	for (int i = 0; i < N; i++)
+		p[i] = 'A' + i;
+	p[N] = 0;
 }
 
-int main(){
+int main()
+{
 	char *p;
-	int N;	
+	int N;
 	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
+	cin >> N;
+	myString(p, N);
+	cout << p;
+	delete[] p;
 	return 0;
 }
--- lab17_3.cpp
+++ lab17_3.cpp
@@ -1,39 +1,48 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 using namespace std;
 
-______________ randomVector(unsigned int N){	
-    vector<int> v;
-    for(unsigned int i = 0; i < N; i++) v.______________(rand()%10);
-    ______________;
+vector<int> randomVector(unsigned int N)
+{
+	vector<int> v(N);
+	for (unsigned int i = 0; i < N; i++)
+		v[i] = (rand() % 10);
+	return v;
 }
 
-______________ showVector(______________){
+void showVector(vector<int> a)
+{
 	cout << "[";
-	for(unsigned int i = 0; _____________________; i++){
-		cout << _________________;
-		if(_________________) cout << "]";
-		else cout << " ";
+	for (unsigned int i = 0; i < a.size(); i++)
+	{
+		cout << a[i];
+		if (i == a.size() - 1)
+			cout << "]";
+		else
+			cout << " ";
 	}
 }
 
-_________________ dotProduct(_____________________________){
+int dotProduct(vector<int> a, vector<int> b)
+{
 	int sum = 0;
-	for(unsigned int i = 0; _____________________; i++) sum += _________________;	
-	_________________;
+	for (unsigned int i = 0; i < a.size(); i++)
+		sum += a[i] * b[i];
+	return sum;
 }
 
-int main(){
+int main()
+{
 	srand(time(0));
 	vector<int> x = randomVector(5);
 	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
+	int z = dotProduct(x, y);
 	showVector(x);
 	cout << " . ";
 	showVector(y);
 	cout << " = " << z;
-	
+
 	return 0;
 }
